bafe593f27139d48ca0f46ead0b1b8bb73d8903e
HeJiaomy <121916986@qq.com>
2019-01-08 23:10:44 +0800

    实现子View的动画

--- app/src/main/java/com/example/hejiao/qqslidemenu/DragLayout.java
+++ app/src/main/java/com/example/hejiao/qqslidemenu/DragLayout.java
@@ -1,9 +1,13 @@
 package com.example.hejiao.qqslidemenu;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.support.annotation.NonNull;
+import android.support.v4.graphics.ColorUtils;
+import android.support.v4.view.ViewCompat;
 import android.support.v4.widget.ViewDragHelper;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.FrameLayout;
@@ -124,19 +128,74 @@ public class DragLayout extends FrameLayout {
         @Override
         public void onViewPositionChanged(@NonNull View changedView, int left, int top, int dx, int dy) {
             super.onViewPositionChanged(changedView, left, top, dx, dy);
-            if (changedView== blueView){
+            if (changedView == blueView) {
                 //红色控件跟随执行
-                redView.layout(redView.getLeft()+dx,redView.getTop()+dy,redView.getRight()+dx,redView.getBottom()+dy);
+                redView.layout(redView.getLeft() + dx, redView.getTop() + dy, redView.getRight() + dx, redView.getBottom() + dy);
             }
-            if (changedView== redView){
+            if (changedView == redView) {
                 //蓝色子控件跟随移动
-                blueView.layout(blueView.getLeft()+dx,blueView.getTop()+dy,blueView.getRight()+dx,blueView.getBottom()+dy);
+                blueView.layout(blueView.getLeft() + dx, blueView.getTop() + dy, blueView.getRight() + dx, blueView.getBottom() + dy);
             }
+
+            //计算移动百分比
+            float fraction = changedView.getLeft() * 1f / (getMeasuredWidth() - changedView.getMeasuredWidth());
+            Log.e("fraction", String.valueOf(fraction));
+            //执行伴随动画
+            executeAnim(fraction);
+        }
+
+        private void executeAnim(float fraction) {
+            //缩放
+//            redView.setScaleX(1 + 0.5f * fraction);
+//            redView.setScaleY(1 + 0.5f * fraction);
+
+            //旋转
+            redView.setRotationY(360 * fraction);
+            blueView.setRotationY(360 * fraction);
+
+            //平移
+//            redView.setTranslationX(120 * fraction);
+            //透明
+//            redView.setAlpha(1 - fraction);
+
+            //颜色渐变
+            redView.setBackgroundColor((Integer) ColorUtil.evaluateColor(fraction, Color.RED, Color.GREEN));
+            setBackgroundColor((Integer) ColorUtil.evaluateColor(fraction, Color.LTGRAY, Color.YELLOW));
         }
 
         @Override
         public boolean tryCaptureView(@NonNull View child, int pointerId) {
             return child == blueView || child == redView;
         }
+
+        /**
+         * 手指抬起的时候执行
+         * @param releasedChild 当前触摸的view
+         * @param xvel X方向移动速度 正：向右移动  负：向左移动
+         * @param yvel Y方向移动速度
+         */
+        @Override
+        public void onViewReleased(@NonNull View releasedChild, float xvel, float yvel) {
+            super.onViewReleased(releasedChild, xvel, yvel);
+            int centerLeft = getMeasuredWidth() / 2;
+            if (releasedChild.getLeft() < centerLeft) {
+                //向左边缓慢滑动
+                viewDragHelper.smoothSlideViewTo(releasedChild, 0, releasedChild.getTop());
+                ViewCompat.postInvalidateOnAnimation(DragLayout.this);
+            } else {
+                //向右边缓慢滑动
+                viewDragHelper.smoothSlideViewTo(releasedChild, getMeasuredWidth() - releasedChild.getMeasuredWidth(),
+                        releasedChild.getTop());
+                ViewCompat.postInvalidateOnAnimation(DragLayout.this);
+            }
+        }
     };
+
+    @Override
+    public void computeScroll() {
+        super.computeScroll();
+        if (viewDragHelper.continueSettling(true)) {
+            ViewCompat.postInvalidateOnAnimation(DragLayout.this);
+        }
+    }
 }
