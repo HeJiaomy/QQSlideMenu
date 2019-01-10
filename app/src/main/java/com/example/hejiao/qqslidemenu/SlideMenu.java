75829d9a908421b7d8ff4b0cdbe6ea16d0500a78
HeJiaomy <121916986@qq.com>
2019-01-10 22:55:35 +0800

    SlideMenu 40%

--- app/src/main/java/com/example/hejiao/qqslidemenu/SlideMenu.java
+++ app/src/main/java/com/example/hejiao/qqslidemenu/SlideMenu.java
@@ -2,8 +2,10 @@ package com.example.hejiao.qqslidemenu;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
+import android.support.v4.view.ViewCompat;
 import android.support.v4.widget.ViewDragHelper;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.FrameLayout;
@@ -13,6 +15,7 @@ public class SlideMenu extends FrameLayout {
     private View mainView;
     private View menuView;
     private ViewDragHelper viewDragHelper;
+    private double dragRange;
 
     public SlideMenu(Context context) {
         super(context);
@@ -29,18 +32,19 @@ public class SlideMenu extends FrameLayout {
         init();
     }
 
-    private void init(){
-        viewDragHelper= ViewDragHelper.create(this,cb);
+    private void init() {
+        viewDragHelper = ViewDragHelper.create(this, cb);
     }
+
     @Override
     protected void onFinishInflate() {
         super.onFinishInflate();
         //异常处理
-        if (getChildCount()!= 2){
+        if (getChildCount() != 2) {
             throw new IllegalArgumentException("SlideMenu only have 2 children!");
         }
         menuView = getChildAt(0);
-        mainView= getChildAt(1);
+        mainView = getChildAt(1);
     }
 
     @Override
@@ -54,10 +58,74 @@ public class SlideMenu extends FrameLayout {
         return true;
     }
 
-    private ViewDragHelper.Callback cb= new ViewDragHelper.Callback() {
+    /**
+     * 该方法在onMeasure完成后执行，可以初始化自己和子view的宽高
+     *
+     * @param w
+     * @param h
+     * @param oldw
+     * @param oldh
+     */
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        dragRange = getMeasuredWidth() * 0.6;
+    }
+
+    private ViewDragHelper.Callback cb = new ViewDragHelper.Callback() {
         @Override
         public boolean tryCaptureView(@NonNull View child, int pointerId) {
-            return false;
+            return child == menuView || child == mainView;
+        }
+
+        //控制移动范围
+        @Override
+        public int getViewHorizontalDragRange(@NonNull View child) {
+            return (int) dragRange;
+        }
+
+        //控制View在水平方向上移动
+        @Override
+        public int clampViewPositionHorizontal(@NonNull View child, int left, int dx) {
+            if (child == mainView) {
+                if (left < 0) left = 0; //限制mainView的左边界
+                if (left > dragRange) left = (int) dragRange;//限制mainView的右边界
+            }
+            return left;
+        }
+
+        //childView位置改变时执行
+        @Override
+        public void onViewPositionChanged(@NonNull View changedView, int left, int top, int dx, int dy) {
+            if (changedView == menuView) {
+                Log.e("dx", String.valueOf(dx));
+                //固定menuView
+                menuView.layout(0, 0, (int) dragRange, menuView.getMeasuredHeight());
+                int newLeft = mainView.getLeft() + dx;
+                if (newLeft < 0) newLeft = 0;
+                if (newLeft > dragRange) newLeft = (int) dragRange;
+                mainView.layout(newLeft, mainView.getTop() + dy, mainView.getRight() + dx, mainView.getBottom() + dy);
+            }
+            super.onViewPositionChanged(changedView, left, top, dx, dy);
+        }
+
+        @Override
+        public void onViewReleased(@NonNull View releasedChild, float xvel, float yvel) {
+            super.onViewReleased(releasedChild, xvel, yvel);
+            if (mainView.getLeft() < dragRange / 2) {
+                viewDragHelper.smoothSlideViewTo(mainView, 0, mainView.getTop());
+            } else {
+                viewDragHelper.smoothSlideViewTo(mainView, (int) dragRange, mainView.getTop());
+            }
+            ViewCompat.postInvalidateOnAnimation(SlideMenu.this);
         }
     };
+
+    @Override
+    public void computeScroll() {
+        super.computeScroll();
+        if (viewDragHelper.continueSettling(true)) {
+            ViewCompat.postInvalidateOnAnimation(SlideMenu.this);
+        }
+    }
 }
