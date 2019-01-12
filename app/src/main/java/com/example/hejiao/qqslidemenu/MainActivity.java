1c78927bfbdeff94186ceb38bf5c56a1de37b6cd
HeJiaomy <121916986@qq.com>
2019-01-12 23:25:11 +0800

    填充adapter

--- app/src/main/java/com/example/hejiao/qqslidemenu/MainActivity.java
+++ app/src/main/java/com/example/hejiao/qqslidemenu/MainActivity.java
@@ -1,7 +1,14 @@
 package com.example.hejiao.qqslidemenu;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ListView mainListView = findViewById(R.id.main_listview);
+        ListView menuListView = findViewById(R.id.menu_listview);
+        mainListView.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, Constant.NAMES));
+        menuListView.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, Constant.sCheeseStrings) {
+            @NonNull
+            @Override
+            public View getView(int position, View convertView, @NonNull ViewGroup parent) {
+                TextView textView = (TextView) super.getView(position, convertView, parent);
+                textView.setTextColor(Color.WHITE);
+                return textView;
+            }
+        });
     }
 }
